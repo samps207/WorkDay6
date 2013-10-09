c3370c7155dff8dcd9027efe3757b2f1f746710d
idsc3101 <idsc3101@gmail.com>
2013-10-09 15:15:04 -0500

    Work Day 6

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
 
+        Dim num As Integer
+        Do While (num <= 2)
+            lstResult.Items.Add("Number is " & num ^ 2)
+            num = num + 1
+        Loop
+
+        lstResult.Items.Add("Not implemented yet")
     End Sub
 End Class
